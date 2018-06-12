b45a678304d964ea262e8cb8307d81311765e54d
Liwei Huang <aa583045912@gmail.com>
2018-06-12 19:59:30 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+/* global $ */
+//forever faithful Color Palette
+$(document).ready(function(){
+    
+var me = $("#main");
+var about = $("#about");
+var port = $("#port");
+var contact = $("#contact");
+var all = $(".footer");
+
+function hideContent(){
+    $(".stuff").hide();
+}
+hideContent();
+$(".aboutme").hide();
+
+all.click(function(){
+    $(".aboutme").show();
+});
+
+$("#image").click(function(){
+    hideContent();
+    $(".aboutme").hide();
+});
+
+me.click(function(){
+    hideContent();
+    $("#mainCont").show();
+});
+
+about.click(function(){
+    hideContent();
+    console.log("yes");
+    $("#aboutMe").show();
+});
+
+port.click(function(){
+    hideContent();
+    $("#portfolio").show();
+});
+
+contact.click(function(){
+    hideContent();
+    $("#contacter").show();
+});
+
+me.hover(function(){
+    $("#mainText").show();
+});
+
+about.hover(function(){
+    $("#aboutText").show();
+});
+
+port.hover(function(){
+    $("#portText").show();
+});
+
+contact.hover(function(){
+    $("#contText").show();
+});
+
+
+
+
+
+
+
+
+
+
+
+});
